fc2948217be526b4a72b53f6c407c9c8be2fa64f
alexomon018 <aleksa.mitic5859@gmail.com>
2020-12-19 12:20:22 +0100

    finished navbar and sidebar component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import HeaderOption from '../HeaderOption/HeaderOption'
+import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import SuppervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import { BusinessCenter, Chat, Notifications } from '@material-ui/icons'
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <img
+          src='https://www.flaticon.com/svg/static/icons/svg/174/174857.svg'
+          alt=''
+        />
+
+        <div className='header__search'>
+          <SearchIcon />
+          <input type='text' />
+        </div>
+      </div>
+      <div className='header__right'>
+        <HeaderOption Icon={HomeIcon} title='Home' />
+        <HeaderOption Icon={SuppervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={Chat} title='Messaging' />
+        <HeaderOption Icon={Notifications} title='Notifications' />
+        <HeaderOption Icon={BusinessCenter} title='Jobs' />
+        <HeaderOption
+          avatar='https://pickaface.net/gallery/avatar/unr_random_180527_1151_2bcb7h9.png'
+          title='me'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Header
